3e289f653490ccfae78484b56c7f2dcb0eba6f7b
Caden Wehmeyer <caden@wehmeyers.com>
2026-09-05 13:33:43 -0400

    Update image path in albumThingy3.py

--- album shuffler/albumThingy3.py
+++ album shuffler/albumThingy3.py
@@ -73,4 +73,4 @@ if musicChoice:
 if st.button("wanna add albums? click here"):
     webbrowser.open("https://docs.google.com/forms/d/e/1FAIpQLScHWov8uIiYv_lZKnfjKkjdjZxGSIihmab4VLbwQvvlmmsiUw/viewform?usp=publish-editor")
 #python3 -m streamlit run "albumThingy3.py" 
-st.image("/Users/caden/Downloads/album shuffler/images/just-wanted-to-drop-by-and-say-this-is-my-favorite-gif-of-v0-jzr6kdt6bb771.gif.png")
+st.image("images/just-wanted-to-drop-by-and-say-this-is-my-favorite-gif-of-v0-jzr6kdt6bb771.gif.png")
